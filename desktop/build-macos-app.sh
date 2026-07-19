61af98d7f7f8d046278bb622e7eda625cf6e0f6e
DaqingZhao-ops <DaqingZhao-ops@users.noreply.github.com>
2026-07-19 13:21:23 -0700

    Add dark green MRM macOS app icon

--- desktop/build-macos-app.sh
+++ desktop/build-macos-app.sh
@@ -25,6 +25,30 @@ swiftc \
   -o "$MACOS/MarketRiskModels"
 
 cp "$ROOT/desktop/Info.plist" "$CONTENTS/Info.plist"
+
+ICON_WORK="$(mktemp -d)"
+ICONSET="$ICON_WORK/MarketRiskModels.iconset"
+mkdir -p "$ICONSET"
+for SPEC in \
+  "16 icon_16x16.png" \
+  "32 icon_16x16@2x.png" \
+  "32 icon_32x32.png" \
+  "64 icon_32x32@2x.png" \
+  "128 icon_128x128.png" \
+  "256 icon_128x128@2x.png" \
+  "256 icon_256x256.png" \
+  "512 icon_256x256@2x.png" \
+  "512 icon_512x512.png" \
+  "1024 icon_512x512@2x.png"
+do
+  SIZE="${SPEC%% *}"
+  NAME="${SPEC#* }"
+  sips -s format png -z "$SIZE" "$SIZE" "$ROOT/desktop/AppIcon.jpg" \
+    --out "$ICONSET/$NAME" >/dev/null
+done
+iconutil -c icns "$ICONSET" -o "$RESOURCES/MarketRiskModels.icns"
+rm -rf "$ICON_WORK"
+
 cat > "$RESOURCES/launcher-config.json" <<JSON
 {
   "projectRoot": "$ROOT",
